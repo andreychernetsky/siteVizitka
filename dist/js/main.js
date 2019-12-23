afc540587fb9492e77af3f7b6f5b7b57f7613d05
andric <rodomir3119841985@gmail.com>
2019-12-23 19:43:36 +0300

    Duist

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+// burger
+(()=> {
 
 let btn = document.querySelector('.menu-btn');
 let nav = document.getElementById('nav');
@@ -5,3 +7,18 @@ let nav = document.getElementById('nav');
 btn.addEventListener('click',function () {
   nav.classList.toggle('active');
 });
+
+})();
+
+// accordion
+(()=>{
+  const acc = document.getElementsByClassName("accordion__header");
+  let i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("show");
+    }
+  }
+})();
